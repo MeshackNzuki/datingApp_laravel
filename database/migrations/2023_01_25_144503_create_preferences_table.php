92dcd77980b5cc1dac699c1595d99e512f47d2d5
Mesh <meshkaka1@gmail.com>
2023-01-25 10:09:39 -0500

    onboard form handling

--- database/migrations/2023_01_25_144503_create_preferences_table.php
+++ database/migrations/2023_01_25_144503_create_preferences_table.php
@@ -15,6 +15,23 @@ return new class extends Migration
     {
         Schema::create('preferences', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->string('intelligence')->nullable();
+            $table->string('goodlooks')->nullable();
+            $table->string('humor')->nullable();
+            $table->string('bedroom_skills')->nullable();
+            $table->string('boldness')->nullable();
+            $table->string('money')->nullable();
+            $table->string('power')->nullable();
+            $table->string('flirtatiousness')->nullable();
+            $table->string('wit')->nullable();
+            $table->string('endowment')->nullable();
+            $table->string('ass')->nullable();
+            $table->string('petite')->nullable();
+            $table->string('big_boobs')->nullable();
             $table->timestamps();
         });
     }
